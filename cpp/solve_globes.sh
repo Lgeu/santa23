0665ffd757eca95155580e797d557fa01e908bca
kibuna <task.aizawa@gmail.com>
2024-01-27 15:26:54 +0900

    add n_thread arg to script

--- cpp/solve_globes.sh
+++ cpp/solve_globes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ "$#" -ne 6 ]; then
-    echo "usage: $0 start_idx end_idx beam_width max_cost max_depth max_conjugate_depth"
+if [ "$#" -ne 7 ]; then
+    echo "usage: $0 start_idx end_idx beam_width max_cost max_depth max_conjugate_depth n_threads"
     exit 1
 fi
 
@@ -10,5 +10,6 @@ end=$2
 
 for (( i=start; i<=end; i++ ))
 do
-    ./solve.out $i $3 $4 $5 $6
+    ./solve.out $i $3 $4 $5 $6 $7
+    sleep 1
 done
